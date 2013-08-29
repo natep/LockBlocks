e9ba8e94966cbe5d4e640626792bd3f66eb32a75
Nate Petersen <nate@digitalrickshaw.com>
2013-08-29 16:12:13 -0500

    Initial Commit

---
+++ LockBlocks/DRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DRAppDelegate.h
+//  LockBlocks
+//
+//  Created by Nate Petersen on 8/29/13.
+//  Copyright (c) 2013 Digital Rickshaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LockBlocks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LockBlocks
+//
+//  Created by Nate Petersen on 8/29/13.
+//  Copyright (c) 2013 Digital Rickshaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DRAppDelegate class]));
+	}
+}
